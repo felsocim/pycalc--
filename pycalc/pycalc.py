d460371c754d756b99ece511470fd7d98dadf44f
Marek Felšöci <marek.felsoci@uvsq.fr>
2026-02-24 23:07:05 +0100

    Revert "Use a class"
    This reverts commit 0dbee7c78537967ca2d6083d3ed2eb5503c884ca.

--- pycalc/pycalc.py
+++ pycalc/pycalc.py
@@ -1,9 +1,8 @@
-class PyCalc:
-  def add(a, b):
-    pass
-  def subtract(a, b):
-    pass
-  def multiply(a, b):
-    pass
-  def divide(a, b):
-    pass
+def add(a, b):
+  pass
+def subtract(a, b):
+  pass
+def multiply(a, b):
+  pass
+def divide(a, b):
+  pass
