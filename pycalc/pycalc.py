c756adcbf6604c39526b285af5c8160780412f8b
Marek Felšöci <marek.felsoci@uvsq.fr>
2026-02-24 22:28:29 +0100

    Use a class

--- pycalc/pycalc.py
+++ pycalc/pycalc.py
@@ -1,8 +1,9 @@
-def add(a, b):
-  pass
-def subtract(a, b):
-  pass
-def multiply(a, b):
-  pass
-def divide(a, b):
-  pass
+class PyCalc:
+  def add(a, b):
+    pass
+  def subtract(a, b):
+    pass
+  def multiply(a, b):
+    pass
+  def divide(a, b):
+    pass
