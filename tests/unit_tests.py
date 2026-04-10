e33cdd3603a8b4c353884d77ade4b5e8e22d0287
Marek Felšöci <marek.felsoci@uvsq.fr>
2026-04-10 16:39:20 +0200

    Tester la soustraction

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -13,6 +13,17 @@ class PyCalcUnitTests(unittest.TestCase):
       with self.subTest(l=l, r=r, e=e):
         got = calculator.add(l, r)
         self.assertEqual(got, e)
+  def test_subtract(self):
+    left = [12, 44, 7, 1, 54, 0, 0, 0, -12, -44, -7, -1, -54]
+    right = [2, 44, 17, 0, -22, 23, 0, -5, 45, 0, -10, -1, -22]
+    expected = [10, 0, -10, 1, 76, -23, 0, 5, -57, -44, 3, 0, -32]
+    for i in range(len(left)):
+      l = left[i]
+      r = right[i]
+      e = expected[i]
+      with self.subTest(l=l, r=r, e=e):
+        got = calculator.subtract(l, r)
+        self.assertEqual(got, e)
 
 if __name__ == '__main__':
   unittest.main()
