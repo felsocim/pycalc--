e21f27935bcc1e381dd30a909c5062e140cb7c49
Marek Felšöci <marek.felsoci@uvsq.fr>
2026-04-10 16:23:25 +0200

    Implémenter l'addition

--- pycalc/pycalc.py
+++ pycalc/pycalc.py
@@ -1,5 +1,15 @@
 def add(a, b):
-  pass
+  res = a
+  if b > 0:
+    while (b - 1) >= 0:
+      b -= 1
+      res += 1
+  elif b < 0:
+    while (b + 1) <= 0:
+      b += 1
+      res -= 1
+  return res
+
 def subtract(a, b):
   pass
 def multiply(a, b):
